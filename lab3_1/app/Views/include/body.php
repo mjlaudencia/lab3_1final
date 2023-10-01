64e922d7ff787f9e66949366a009c47bd5404112
mjlaudencia <laudenciamj3@gmail.com>
2023-10-01 16:13:28 +0800

    rawr

---
+++ lab3_1/app/Views/include/body.php
@@ -0,0 +1,107 @@
+<?=$this->include('include/header') ?>
+<div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                            <div class="center-desk">
+                                <div class="logo">
+                                    <a href="index.html"><img src="images/logo.png" alt="#"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <div class="menu-area">
+                            <div class="limit-box">
+                                <nav class="main-menu">
+                                    <ul class="menu-area-main">
+                                        <li class="active"> <a href="index.html">Home</a> </li>
+                                        <li> <a href="about.html">About</a> </li>
+                                        <li><a href="brand.html">Brand</a></li>
+                                        <li><a href="special.html">Specials</a></li>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                        <li class="last">
+                                            <a href="#"><img src="images/search_icon.png" alt="icon" /></a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 offset-md-6">
+                        <div class="location_icon_bottum">
+                            <ul>
+                                <li><img src="icon/call.png" />(+71)9876543109</li>
+                                <li><img src="icon/email.png" />demo@gmail.com</li>
+                                <li><img src="icon/loc.png" />Location</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end header inner -->
+    </header>
+    <!-- end header -->
+    <section class="slider_section">
+        <div id="myCarousel" class="carousel slide banner-main" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="first-slide" src="images/banner.jpg" alt="First slide">
+                    <div class="container">
+                        <div class="carousel-caption relative">
+                            <span>All New Phones </span>
+                            <h1>up to 25% Flat Sale</h1>
+                            <p>It is a long established fact that a reader will be distracted by the readable content
+                                <br> of a page when looking at its layout. The point of using Lorem Ipsum is that</p>
+                            <a class="buynow" href="#">Buy Now</a>
+                            <ul class="social_icon">
+                                <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="second-slide" src="images/banner.jpg" alt="Second slide">
+                    <div class="container">
+                        <div class="carousel-caption relative">
+                            <span>All New Phones </span>
+                            <h1>up to 25% Flat Sale</h1>
+                            <p>It is a long established fact that a reader will be distracted by the readable content
+                                <br> of a page when looking at its layout. The point of using Lorem Ipsum is that</p>
+                            <a class="buynow" href="#">Buy Now</a>
+                            <ul class="social_icon">
+                                <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="third-slide" src="images/banner.jpg" alt="Third slide">
+                    <div class="container">
+                        <div class="carousel-caption relative">
+                            <span>All New Phones </span>
+                            <h1>up to 25% Flat Sale</h1>
+                            <p>It is a long established fact that a reader will be distracted by the readable content
+                                <br> of a page when looking at its layout. The point of using Lorem Ipsum is that</p>
+                            <a class="buynow" href="#">Buy Now</a>
+                            <ul class="social_icon">
+                                <li> <a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li> <a href="#"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <i class='fa fa-angle-left'></i>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <i class='fa fa-angle-right'></i>
+            </a>
+        </div>
